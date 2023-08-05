23d5990fe826fb19a7f247a00ffb2ff905022246
prettyturtle <unifconti@gmail.com>
2023-08-06 01:39:50 +0900

    Initial Commit

---
+++ yc_Restart/yc_RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  yc_RestartApp.swift
+//  yc_Restart
+//
+//  Created by yc on 2023/08/06.
+//
+
+import SwiftUI
+
+@main
+struct yc_RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
